c7ebc68e8660c112f485c012890ad702bb19824b
Mai Huy Hoang <maihuyhoang97@gmail.com>
2021-02-27 15:33:25 +0700

    Finish uthread

--- user/uthread.c
+++ user/uthread.c
@@ -11,14 +11,34 @@
 #define MAX_THREAD  4
 
 
+struct thread_context {
+  uint64 ra;
+  uint64 sp;
+
+  // callee-saved
+  uint64 s0;
+  uint64 s1;
+  uint64 s2;
+  uint64 s3;
+  uint64 s4;
+  uint64 s5;
+  uint64 s6;
+  uint64 s7;
+  uint64 s8;
+  uint64 s9;
+  uint64 s10;
+  uint64 s11;
+};
+
 struct thread {
-  char       stack[STACK_SIZE]; /* the thread's stack */
-  int        state;             /* FREE, RUNNING, RUNNABLE */
+  char                  stack[STACK_SIZE]; /* the thread's stack */
+  int                   state;             /* FREE, RUNNING, RUNNABLE */
+  struct thread_context context;
 
 };
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
-extern void thread_switch(uint64, uint64);
+extern void thread_switch(struct thread_context* old, struct thread_context* new);
               
 void 
 thread_init(void)
@@ -54,6 +74,12 @@ thread_schedule(void)
     printf("thread_schedule: no runnable threads\n");
     exit(-1);
   }
+  else
+  {
+    //for (int i = 1; i < MAX_THREAD; i++)
+    //  printf("thread %d, state = %d\n", i, all_thread[i].state);
+    //printf("next thread = %d\n", (next_thread - all_thread));
+  }
 
   if (current_thread != next_thread) {         /* switch threads?  */
     next_thread->state = RUNNING;
@@ -63,6 +89,7 @@ thread_schedule(void)
      * Invoke thread_switch to switch from t to next_thread:
      * thread_switch(??, ??);
      */
+    thread_switch(&t->context, &next_thread->context);
   } else
     next_thread = 0;
 }
@@ -76,13 +103,17 @@ thread_create(void (*func)())
     if (t->state == FREE) break;
   }
   t->state = RUNNABLE;
-  // YOUR CODE HERE
+
+  memset(&t->context, 0, sizeof(t->context));
+  t->context.ra = (uint64)func;
+  t->context.sp = (uint64)t->stack + STACK_SIZE;
 }
 
 void 
 thread_yield(void)
 {
   current_thread->state = RUNNABLE;
+  //printf("current thread = %d, state = %d\n", current_thread - all_thread, current_thread->state);
   thread_schedule();
 }
 
